401a8a670548c3673a4f69800f5f420a965db6f7
BurningFalls <sherlocky31415@gmail.com>
2023-12-07 16:50:55 +0900

    feat: bert/korean, bert 추가/english api

--- app/controllers.py
+++ app/controllers.py
@@ -16,8 +16,8 @@ def predict_sentiment(sentence, index):
     elif index == 'E':
         preds_list = text_classifier_english(sentence)[0]
 
-    predicted_label = int(re.sub(r'[^0-9]', '', preds_list[0]['label']))
-    # predicted_score = preds_list[0]['score']
+    sorted_preds_list = sorted(preds_list, key=lambda x: x['score'], reverse=True)
+    predicted_label = int(re.sub(r'[^0-9]', '', sorted_preds_list[0]['label']))
 
     real_label = ""
     if index == 'K':
@@ -28,9 +28,38 @@ def predict_sentiment(sentence, index):
     return real_label
 
 
+def show_bert_result(sentence, index):
+    preds_list = []
+    if index == 'K':
+        preds_list = text_classifier_korean(sentence)[0]
+    elif index == 'E':
+        preds_list = text_classifier_english(sentence)[0]
+
+    sorted_preds_list = sorted(preds_list, key=lambda x: x['score'], reverse=True)
+
+    results = []
+
+    for i in range(0, len(sorted_preds_list)):
+        predicted_label = int(re.sub(r'[^0-9]', '', sorted_preds_list[i]['label']))
+        predicted_score = round(sorted_preds_list[i]['score'], 4)
+
+        real_label = ""
+        if index == 'K':
+            real_label = labels_korean[predicted_label]
+        elif index == 'E':
+            real_label = labels_english[predicted_label]
+        results.append({
+            "label": real_label,
+            "score": predicted_score
+        })
+
+    return results
+
+
 def split_sentences(text):
     text = text.replace('...', '☉')
     sentences = re.split(r'(?<=[.!?☉])', text)
+
     for i in range(0, len(sentences)):
         sentences[i] = sentences[i].replace('☉', '...')
     sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
@@ -58,6 +87,17 @@ def classify_korean_text():
         return jsonify({'error': 'Missing text in request'}), 400
 
 
+@app.route('/bert/korean', methods=['POST'])
+def using_bert_korean():
+    if 'text' in request.json:
+        sentence = request.json['text']
+        results = show_bert_result(sentence, 'K')
+
+        return jsonify(results)
+    else:
+        return jsonify({'error': 'Missing sentence in request'}), 400
+
+
 @app.route('/classify/english', methods=['POST'])
 def classify_english_text():
     if 'text' in request.json:
@@ -76,3 +116,14 @@ def classify_english_text():
         return jsonify(results)
     else:
         return jsonify({'error': 'Missing text in request'}), 400
+
+
+@app.route('/bert/english', methods=['POST'])
+def using_bert_english():
+    if 'text' in request.json:
+        sentence = request.json['text']
+        results = show_bert_result(sentence, 'E')
+
+        return jsonify(results)
+    else:
+        return jsonify({'error': 'Missing sentence in request'}), 400
--- app/model_loader.py
+++ app/model_loader.py
@@ -8,7 +8,7 @@ text_classifier_korean = TextClassificationPipeline(
     tokenizer=loaded_tokenizer_korean,
     model=loaded_model_korean,
     framework='tf',
-    top_k=1,
+    top_k=4,
     truncation=True
 )
 
@@ -21,6 +21,6 @@ text_classifier_english = TextClassificationPipeline(
     tokenizer=loaded_tokenizer_english,
     model=loaded_model_english,
     framework='tf',
-    top_k=1,
+    top_k=4,
     truncation=True
 )
